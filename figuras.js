2f43f9c12bf069f469c7cf8288c2efca3136a85c
Geramx095 <shinjinobu28@gmail.com>
2021-09-29 21:24:36 -0500

    Integramos js a html, agregue los calculos de las figuras y cambie a funciones los calculos

--- figuras.js
+++ figuras.js
@@ -1,1 +1,58 @@
-console.log("Hello, World!");
+// Codigo del cuadrado
+
+console.group("Cuadrado");
+function perimetroCuadrado(lado) {
+    return  lado * 4;
+}
+function areaCuadrado(lado){
+    return lado * lado;
+}
+console.groupEnd();
+
+
+// Codigo del Triangulo
+
+console.group("Triangulo");
+
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
+function areaTriangulo(base, altura,){
+    return (base * altura) / 2;
+}
+console.groupEnd();
+
+
+
+// Codigo del circulo
+
+console.group("Circulo");
+// Radio
+//function radioCirculo(radio){
+   // return radio;
+//}
+
+//Diametro
+function diametroCirculo(radio){
+    return radio * 2;
+}
+
+
+//PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+
+//Circunferencia
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+//
+function areaCirculo(radio){
+    return PI * (radio * radio);
+}
+
+console.groupEnd();
+
