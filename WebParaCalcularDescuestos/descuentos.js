d9c6d9f0c6803ee8e8cb0253991c79eac5f2d221
Geramx095 <shinjinobu28@gmail.com>
2022-03-14 23:00:04 -0600

    Crear pagina web para calcular descuuentos

---
+++ WebParaCalcularDescuestos/descuentos.js
@@ -0,0 +1,21 @@
+//const precioOriginal = 100;
+//const descuento = 18;
+
+
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+
+
+    return precio * porcentajePrecioConDescuento / 100;
+}
+
+
+
+//console.log({
+//  precioOriginal,
+// descuento,
+//porcentajePrecioConDescuento,
+//precioConDescuento,
+//});
+//
